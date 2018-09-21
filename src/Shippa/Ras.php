74423918c7a8cb667ecb65cdc85431961e05be93
Kris Ager <kris@krisager.co.uk>
2018-09-21 13:35:13 +0100

    Added Ras lookup

---
+++ src/Shippa/Ras.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Shippa;
+
+class Ras
+{
+
+	protected 	$ras_fetch = false,
+				$api_key = null,
+				$ras_type = false,
+				$ras_carrier_data = [];
+
+	public function __construct($key = '')
+	{
+		if($key) {
+			$this->api_key = $key;
+		}
+	}
+
+
+	public function getRas($country_code, $postcode, $carrier = '')
+	{
+		$url = "http://api.shippa.test/";
+
+		if($carrier) {
+			$url .= "/{$carrier}";
+		}
+
+		$url .= "/ras/{$country_code}/{$postcode}";
+
+
+		$c = curl_init();
+		curl_setopt($c, CURLOPT_URL, $url);
+		curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($c, CURLOPT_HTTPHEADER, [
+			'Authorization: Bearer ' . $this->api_key,
+			'Accept: application/json',
+			'Content-Type: application/json'
+		]);
+
+		$ret= curl_exec ($c);
+		curl_close($c);
+		$this->ras_fetch = true;
+		$ras = json_decode($ret);
+
+		if("none" !== $ras[0]) {
+			$this->ras_type = $ras[0];
+			foreach($ras[1] as $carrier_data) {
+				$this->ras_carrier_data[$carrier_data->carrier_name] = $carrier_data;
+			}
+		}
+	}
+
+	public function __call($method, $parameters)
+	{
+		if(!$this->ras_fetch) {
+			throw new \Exception("No RAS lookup data");
+		}
+		$method_pre = substr($method, 0, 3);
+		$carrier =  substr($method, 3);
+		if("has" === $method_pre) {
+			// Check whether carrier has been returned
+			return $this->has(strtolower($carrier));
+		} else if("get" === $method_pre) {
+			// return data associated to carrier
+			return $this->get(strtolower($carrier));
+		} else {
+			throw new \Exception("Method {$method}: not found");
+		}
+
+	}
+
+	public function get($carrier)
+	{
+		$carrier = strtolower($carrier);
+		if($this->has($carrier)) {
+			return $this->ras_carrier_data[$carrier];
+		}
+
+		return false;
+	}
+
+	public function has($carrier)
+	{
+		$carrier = strtolower($carrier);
+		return !empty($this->ras_carrier_data[$carrier]);
+	}
+
+}
